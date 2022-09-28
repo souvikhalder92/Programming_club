d40a9f14f5b2965021c49dc31fe748b1e118c2b9
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-09-29 02:58:55 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import Activity from './component/Activity/Activity';
 import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
        <Activity></Activity>
+       <Footer></Footer>
     </div>
   );
 }
---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            
+            <div className='footer'>
+            <h2>Programming Club</h2>
+            <p><small>All rights reserved.@2022.Souvik Halder.</small></p>
+            </div>
+             
+        </div>
+    );
+};
+
+export default Footer;
