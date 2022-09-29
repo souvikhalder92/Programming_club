e34d6cd252ba119319beba0ec04b42ef8a472839
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-09-29 09:38:27 +0600

    ok

--- src/component/Activity/Activity.js
+++ src/component/Activity/Activity.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import Language from '../Language/Language';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceGrin } from '@fortawesome/free-solid-svg-icons'
 
 import './Activity.css'
 
@@ -28,7 +30,32 @@ const Activity = () => {
             
        
            <div className="info-container">
-             <p>Cart</p>
+            <div className='info'>
+              <h4><FontAwesomeIcon icon={faFaceGrin}></FontAwesomeIcon> Souvik Halder</h4>
+              <p><small>Fullstack Developer</small></p>
+              <p><small>souvikhalder205@gmail.com</small></p>
+            </div>
+            <div className='details'>
+              <div className='details1'>
+                <p>15+</p>
+                <p>Projects</p>
+                </div>
+               <div className="details2">
+                <p>8+
+                </p>
+                <p>Happy clients</p>
+               </div>
+            
+            </div>
+            <div className="break">
+              <h2>Add A Break</h2>
+              <div>
+              <button className='btn1'>10min</button>
+              <button className='btn2'>20min</button>
+              <button className='btn3'>30min</button>
+              <button className='btn4'>40min</button>
+              </div>
+            </div>
            </div>
             
         </div>
